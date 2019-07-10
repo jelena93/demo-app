c9172064a16917f20ae0ef7cb184896239bcede8
javaweb <javaweb@006-12.prod.fon.bg.ac.rs>
2019-07-10 20:15:00 +0200

    Moja klasa

---
+++ src/demo/app/StefanKostic.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo.app;
+
+/**
+ *
+ * @author javaweb
+ */
+public class StefanKostic {
+    String ime = "Stefan";
+    String prezime = "Prezime";
+
+    public StefanKostic() {
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+    
+    
+}
