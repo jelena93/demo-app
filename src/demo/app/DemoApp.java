466f4a776a48a057f12c44b14ef82cff285829ec
jelena93 <sijan.jelena93@gmail.com>
2019-07-10 16:29:03 +0200

    Added Student class

--- src/demo/app/DemoApp.java
+++ src/demo/app/DemoApp.java
@@ -15,7 +15,8 @@ public class DemoApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Student student = new Student("John", "Smith");
+        System.out.println("Student: " + student);
     }
-    
+
 }
