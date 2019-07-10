70a5d86656a3f68861a0bf6eedfbe0a22176dd06
jelena93 <sijan.jelena93@gmail.com>
2019-07-10 19:53:11 +0200

    Added Student

---
+++ src/demo/app/Student.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo.app;
+
+/**
+ *
+ * @author jelena
+ */
+public class Student {
+
+    private String firstName;
+    private String lastName;
+
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "FirstName=" + firstName + ", lastName=" + lastName;
+    }
+
+}
