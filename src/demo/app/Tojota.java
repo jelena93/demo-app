62c520476f07af330da09edf534039f71257280e
java <java@006-28.prod.fon.bg.ac.rs>
2019-07-10 20:15:04 +0200

    Metalik Siva

---
+++ src/demo/app/Tojota.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo.app;
+
+/**
+ *
+ * @author java
+ */
+public class Tojota {
+    
+    private String model;
+    private String boja;
+    private boolean jeLiCukan;
+    
+}
